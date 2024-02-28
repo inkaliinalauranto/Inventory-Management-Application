82816c897081fde506093ed149bef3f129b46aa8
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2024-02-28 13:29:32 +0200

    Gradleen viewmodel-dependencyn lisäys ja LoginScreen-käyttöliittymän ulkoasun luonti.

--- app/src/main/java/com/example/inventorymanagementapplication/LoginScreen.kt
+++ app/src/main/java/com/example/inventorymanagementapplication/LoginScreen.kt
@@ -1,8 +1,71 @@
 package com.example.inventorymanagementapplication
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.inventorymanagementapplication.viewmodel.LoginViewModel
 
+
+/* Builds the UI for the login screen using the LoginScreen composable
+function. Displays a CircularProgressIndicator if the loading argument of
+the loginState is true. Otherwise displays two text fields and a Login
+button. The text in the last text field is masked for security.
+*/
+@Preview
 @Composable
-fun LoginScreen(onLoginClick: () -> Unit) {
+fun LoginScreen() {
+    val loginVM: LoginViewModel = viewModel()
+    Box(modifier = Modifier.fillMaxSize()) {
 
+        when {
+            loginVM.loginState.value.loading -> CircularProgressIndicator(
+                modifier = Modifier.align(
+                    Alignment.Center
+                )
+            )
+            else -> Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    value = loginVM.loginState.value.username,
+                    onValueChange = {
+                        loginVM.setUsername(it)
+                    },
+                    placeholder = {
+                        Text(text = "Username")
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    visualTransformation = PasswordVisualTransformation(),
+                    value = loginVM.loginState.value.password,
+                    onValueChange = {
+                        loginVM.setPassword(it)
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {
+                    loginVM.login()
+                }) {
+                    Text(text = "Login")
+                }
+            }
+        }
+    }
 }
